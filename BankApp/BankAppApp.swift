240099eb8af1d41d90c9ab5d7b9c56db09518bd2
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-05 15:38:50 +0900

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by 飯塚政美 on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
